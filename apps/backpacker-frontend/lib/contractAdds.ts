69931282cadc106f90d5426a22ff872eed3c3cf2
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-11-17 06:26:54 +0700

    token

--- apps/backpacker-frontend/lib/contractAdds.ts
+++ apps/backpacker-frontend/lib/contractAdds.ts
@@ -1,3 +1,4 @@
 export const contractAdds = {
-    minting:"0xa28BE61401D37c646DB97b17b31dF8c53Db16Def"
+    minting:"0xa28BE61401D37c646DB97b17b31dF8c53Db16Def",
+    token: "0x313804B66Bb6d01BC12a4B6BEA569b5D790fC241"
 }
