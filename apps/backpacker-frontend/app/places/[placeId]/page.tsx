f4c041dbbf6ce94625590353d0bddac0c1bd7be7
Risav03 <needleweed03@gmail.com>
2024-11-17 06:26:54 +0700

    works

--- apps/backpacker-frontend/app/places/[placeId]/page.tsx
+++ apps/backpacker-frontend/app/places/[placeId]/page.tsx
@@ -1,21 +1,19 @@
 'use client'
-import { getReviewsByPlace } from "@/lib/hooks/reviewsByPlace.hook";
+import { useReviewsByPlace } from "@/lib/hooks/reviewsByPlace.hook";
 import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { usePathname } from "next/navigation"
 import { useEffect } from "react";
 
 export default function Places(){
 
-    const pathname = usePathname();
-
-    const { primaryWallet } = useDynamicContext()
-
+    const pathname = usePathname()
+    const{reviews, getReviews} = useReviewsByPlace();
 
     useEffect(()=>{
-        getReviewsByPlace(primaryWallet, pathname.split("/")[2])
-    },[primaryWallet])
+        getReviews(pathname.split("/")[2]);
+    },[pathname])
 
     return(
-        <div></div>
+        <div>{reviews}</div>
     )
 }
--- apps/backpacker-frontend/lib/hooks/reviewsByPlace.hook.ts
+++ apps/backpacker-frontend/lib/hooks/reviewsByPlace.hook.ts
@@ -1,5 +1,5 @@
 import { useDynamicContext } from '@dynamic-labs/sdk-react-core'
-import React from 'react'
+import React, { useState } from 'react'
 import { useContractSetup } from './contractSetup.hook'
 import { contractAdds } from '../contractAdds'
 import abi from "@/lib/abis/minting"
@@ -7,18 +7,21 @@ import abi from "@/lib/abis/minting"
 export const useReviewsByPlace = () => {
 
   const { primaryWallet } = useDynamicContext()
+  const[reviews, setReviews] = useState<any>([]);
   
-  async function getPosts(placeId:string){
+  async function getReviews(placeId:string){
     try{
       const contract = await useContractSetup({address: contractAdds.minting, abi, wallet:primaryWallet});
       const res = await contract?.returnURIsByPlace(placeId);
-
-
-      return res;
+      console.log(res);
+      setReviews(res);
     }
     catch(err){
       console.log(err);
     }
 
 }
+
+  return {getReviews, reviews}
+
 }
