83da85610d260b00ab1de4370261ac68de16ea9d
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-11-17 06:50:30 +0700

    search

--- apps/backpacker-frontend/app/review/page.tsx
+++ apps/backpacker-frontend/app/review/page.tsx
@@ -1,11 +1,11 @@
-import { ReviewComponent } from "@/components/review/reviewComponent";
+import CombinedReviewSearch from "@/components/review/reviewComponent"
 
 export default function Home() {
 
   return (
     <div className="flex flex-col bg-white h-screen px-6 py-10 max-md:py-20 max-md:px-4">
       <div className="">
-        <ReviewComponent/>
+        <CombinedReviewSearch/>
       </div>
     </div>
   );
